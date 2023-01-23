1fea8b2aafbb46562bf53a7210f0b2564292b6c6
marinabpl <85500739+marinabpl@users.noreply.github.com>
2023-01-23 14:09:21 +0000

    Add files via upload

---
+++ js/lista.js
@@ -0,0 +1,80 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nota = evento.target.elements['nota']
+    const materia = evento.target.elements['materia']
+    
+
+    const existe = itens.find( elemento => elemento.nota === nota.value )
+
+    const itemAtual = {
+        "materia": materia.value,
+        "nota": nota.value,
+        
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+    nota.value = ""    
+    materia.value = ""
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.nota
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+    
+    novoItem.innerHTML += item.materia
+
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.nota
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
